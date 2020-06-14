2ebce3a3134a486b0a8018fd51bf0e8a4db3e1d4
Mitch Hansen <mitchames@live.com>
2020-06-13 17:26:06 -0700

    added an admin view, changed to comic shop, added css

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,16 @@
 class Order < ApplicationRecord
-    has_many :lineitems, dependent: :destroy
+  has_many :lineitems, dependent: :destroy
 
-    validates :name, :address, :email, presence: true
+  validates :name, :address, :email, presence: true
 	
-	PAYMENT_TYPES = ["check", "credit card", "Venmo", "Paypal"]
+	PAYMENT_TYPES = ["Check", "credit Card", "Venmo", "Paypal"]
 	
-    validates :paytype, inclusion: PAYMENT_TYPES
+  validates :paytype, inclusion: PAYMENT_TYPES
 
-    def add_items_from_cart(cart)
-		cart.lineitems do |item|
-			item.cart_id = nil
-			item.order_id = self.id
-		end
+  def add_items_from_cart(cart)
+    cart.lineitems do |item|
+      item.cart_id = nil
+      item.order_id = self.id
     end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
+  get 'access/new'
+  get 'access/create'
+  get 'access/destroy'
+  get 'admin/index'
+  resources :users
   resources :carts
   resources :lineitems
   resources :orders
+  # resources :access
   get 'shopper/index'
   # get '/', to: 'shopper#index'
   root "shopper#index"  # Better to do this short form rather than above, because you need root
 
   get 'shopper', to: 'shopper#index'
+  get 'admin', to: 'admin#index'
+  get 'login', to: "access#new"
 
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
